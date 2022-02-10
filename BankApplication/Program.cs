5f00aa18db98822c1fdae59c038cb0791bb46fbb
unknown <yoshuah809@gmail.com>
2022-02-10 08:31:30 -0600

    Project initialization.

---
+++ BankApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
